d0f39069cf0d9b6c405f02e8359ea1892bbf0767
ateam <travisCxy>
2024-08-20 12:57:00 +0800

    update code

---
+++ DiffBIR-main/CKPT_PTH.py
@@ -0,0 +1,5 @@
+LLAVA_CLIP_PATH = None
+LLAVA_MODEL_PATH = 'options/llava-v1.5-13b'
+SDXL_CLIP1_PATH = None
+SDXL_CLIP2_CKPT_PTH = None
+#'/opt/data/private/AIGC_pretrain/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
